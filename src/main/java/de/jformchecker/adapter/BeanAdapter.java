7d03a444a07a91beca4638c1d6c5728cf993d068
Jochen Pier <jpier@me.com>
2016-11-18 22:30:09 +0100

    Bean Adapter to read Bean with parameters from request

---
+++ src/main/java/de/jformchecker/adapter/BeanAdapter.java
@@ -0,0 +1,20 @@
+package de.jformchecker.adapter;
+
+/**
+ * provides a way to access a bean that is passed as parameter-value  holder
+ * @author jochen
+ *
+ */
+public class BeanAdapter {
+
+	public String submitted;
+
+	public String getSubmitted() {
+		return submitted;
+	}
+
+	public void setSubmitted(String submitted) {
+		this.submitted = submitted;
+	}
+
+}
