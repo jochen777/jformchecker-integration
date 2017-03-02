355db5f4408b62c4f9cc39d89a2debbb15a87302
Jochen Pier <jpier@me.com>
2017-03-02 23:35:41 +0100

    adapted form-validaton (returns now boolean)

--- src/main/java/de/jformchecker/utils/BeanValidationFormValidator.java
+++ src/main/java/de/jformchecker/utils/BeanValidationFormValidator.java
@@ -14,43 +14,44 @@ import de.jformchecker.criteria.ValidationResult;
 
 /**
  * Form Validtor for Bean-Validation usage
+ * 
  * @author jochen
  *
  */
 public class BeanValidationFormValidator implements FormValidator {
 
 	Object bean;
-	
+
 	public BeanValidationFormValidator(Object o) {
 		bean = o;
 	}
-	
+
 	@Override
-	public void validate(FormCheckerForm form) {
+	public boolean validate(FormCheckerForm form) {
 		try {
 			BeanUtils.fillBean(form, bean);
 		} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		// Move to more central place! (or get it from outside)
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		Validator validator = factory.getValidator();
 		Set<ConstraintViolation<Object>> constraintViolations = validator.validate(bean);
 		System.err.println("JOchen:" + Utils.getDebugOutput(form));
+		boolean formIsOkay = false;
+		if (constraintViolations.isEmpty()) {
+			formIsOkay = true;
+		}
 		constraintViolations.forEach(violation -> {
 			System.err.println("LL:" + violation.getPropertyPath().toString());
 			System.err.println(form.getElement(violation.getPropertyPath().toString()));
-			form.getElement(violation.getPropertyPath().toString()).
-			setValidationResult(ValidationResult.failWithTranslated(violation.getMessage()));
-		}
-				);
-		constraintViolations.forEach(violation -> 
-		System.err.println("::" + violation.getPropertyPath().toString())
-			);
-
-		
+			form.getElement(violation.getPropertyPath().toString())
+					.setValidationResult(ValidationResult.failWithTranslated(violation.getMessage()));
+		});
+		constraintViolations.forEach(violation -> System.err.println("::" + violation.getPropertyPath().toString()));
+		return formIsOkay;
 	}
 
 }
